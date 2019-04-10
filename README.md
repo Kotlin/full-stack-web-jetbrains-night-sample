824a464314f0fe3df6f53527dac6ba20b3bd31cd
Mikhail Kraynov <Mikhail.Kraynov@jetbrains.com>
2019-04-10 04:58:26 +0300

    Step 3. Add kotlin-frontend-plugin
    Add kotlin-frontend-plugin dependency to configure webpack and npm.

--- README.md
+++ README.md
@@ -2,4 +2,20 @@
 
 Full stack application demo written in Kotlin for JetBrains Night Moscow 2019.
 
-`gradle run` runs application.
+### Useful Gradle tasks
+`gradle run` runs webpack dev server and ktor application. You could find logs at `build/logs`.
+
+`gradle stop` stops webpack dev server and ktor application.
+
+`gradle bundle` to create static files bundle.
+
+`gradle webpack-run` runs webpack dev server only.
+
+`gradle webpack-stop` stops webpack dev server only.
+
+`gradle ktor-run` runs ktor application only.
+
+`gradle ktor-stop` stops ktor application only.
+
+### Hosts
+Webpack dev server responding at http://0.0.0.0:8080. Ktor application responding at http://0.0.0.0:8081.
--- src/jvmMain/kotlin/Main.kt
+++ src/jvmMain/kotlin/Main.kt
@@ -8,7 +8,6 @@ import io.ktor.routing.routing
 import kotlinx.css.*
 import kotlinx.css.properties.lh
 import kotlinx.html.*
-import java.io.File
 
 private val globalCss = CSSBuilder().apply {
     body {
@@ -23,17 +22,6 @@ private val globalCss = CSSBuilder().apply {
 }
 
 fun Application.main() {
-    val currentDir = File(".").absoluteFile
-    environment.log.info("Current directory: $currentDir")
-
-    val webDir = listOf(
-        "web",
-        "../src/jsMain/web",
-        "src/jsMain/web"
-    ).map {
-        File(currentDir, it)
-    }.firstOrNull { it.isDirectory }?.absoluteFile ?: error("Can't find 'web' folder for this sample")
-
     routing {
         get("/") {
             call.respondHtml {
@@ -55,18 +43,14 @@ fun Application.main() {
                         id = "js-response"
                         +"Loading..."
                     }
-                    script(src = "/static/require.min.js") {
-                    }
-                    script {
-                        +"require.config({baseUrl: '/static'});\n"
-                        +"require(['/static/kotlin-full-stack-application-demo.js']);"
+                    script(src = "/main.bundle.js") {
                     }
                 }
             }
         }
 
-        static("/static") {
-            files(webDir)
+        static("/") {
+            files("build/bundle")
         }
     }
 }
